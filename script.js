09d5d7f2d78faa2849cb8c9a1ee79040ad44126c
islamsorifulhero <islamsoriful.hero@gmail.com>
2025-08-23 16:36:31 +0600

    Payoo Mobile App

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 //login button functionality
 document.getElementById("loginButton").addEventListener('click', function(e){
     e.preventDefault()
-    console.log('fdslkdsf');
+
+    const mobileNumber = 12345678910;
+    const pin = 1234;
+
+    const mobileNumberValue = document.getElementById('mobile-number').value;
+
+    const mobileNumberConverted = parseInt(mobileNumberValue);
+
+    const pinNumberValue = document.getElementById('pin-number').value;
+
+    const pinNumberValueConverted = parseInt(pinNumberValue);
+
+    if(mobileNumberConverted === mobileNumber && pinNumberValueConverted === pin){
+        window.location.href = "./home.html"
+    }
+    else{
+        alert("Invalid Credentials!")
+    }
 })
